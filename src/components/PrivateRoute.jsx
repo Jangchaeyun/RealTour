121025966825be8525188d748092af3056b39504
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-12-19 20:00:42 +0900

    Create private route and custom hook for protecting the profile page and add the logout functionality

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router';
+import useAuthStatus from '../hooks/useAuthStatus';
+
+export default function PrivateRoute() {
+    const { loggedIn, checkingStatus } = useAuthStatus();
+    if (checkingStatus) {
+        return <h3>로딩중...</h3>;
+    }
+    return loggedIn ? <Outlet /> : <Navigate to="/sign-in"/>
+}
